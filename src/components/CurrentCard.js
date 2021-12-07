0b280dc4cb893aa1dff35573891f10fbb169ec9c
Alexander Vadeboncoeur <asvadeboncoeur@gmail.com>
2021-12-06 22:18:40 -0600

    created react app. creater header layout, will be moved to seperate component. created current forecast card, have not styled. created util functions for date formatting

---
+++ src/components/CurrentCard.js
@@ -0,0 +1,25 @@
+import { getDayOfWeek, getMonthString, formatDay } from "../utils/date";
+
+
+const CurrentCard = ({ location, current, feelsLike, icon, iconAlt }) => {
+    const date = new Date();
+
+    const formattedDate = `${getDayOfWeek(date)}, ${getMonthString(date)} ${formatDay(date)}` 
+    console.log(formattedDate)
+    
+
+
+    return (
+        <div className="current">
+            <div>{formattedDate}</div>
+            <img src={`http://openweathermap.org/img/w/${icon}.png`} alt={iconAlt} />
+            <h2>{location}</h2>
+            <div>Current Temperature: {Math.round(current)} °F</div>
+            <div>Feels like: {Math.round(feelsLike)} °F</div>
+        </div>
+    );
+};
+
+export default CurrentCard;
+
+
